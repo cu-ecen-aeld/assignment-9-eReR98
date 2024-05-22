de311b0fa5bcc03d1907302f1fead2e245a80f75
eReR98 <abruderer7@gmail.com>
2024-05-22 15:21:16 -0600

    Added ldd folder and configs

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,21 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+LDD_VERSION = abf197066aab41820a546955c9225f7e02d1c4c3
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-eReR98.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
