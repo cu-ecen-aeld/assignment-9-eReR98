51dcdd69529494068c88d042e88df00ebea0bebd
eReR98 <abruderer7@gmail.com>
2024-06-06 11:46:36 -0600

    updated configs for working driver

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = '801f459bcaa45323f9512329b3de6d8743124083'
+AESDCHAR_VERSION = '06c73c758da966cca6105a1f54aa0b02bf238497'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
